eb906472da88954bf63a500eae25e1eb315cabf3
nennneko5787 <nennneko5787@gmail.com>
2025-02-02 15:10:02 +0900

    files

--- cogs/sniper.py
+++ cogs/sniper.py
@@ -20,11 +20,19 @@ class SniperCog(commands.Cog):
                 before: discord.Message = message[0]
                 after: discord.Message = message[1]
                 if ctx.channel.id == before.channel.id:
-                    text += f"@{before.author.name} > {before.clean_content} -> {after.clean_content}\n"
+                    files = ""
+                    if message.attachments:
+                        for attachment in message.attachments:
+                            files += f"{attachment.url} "
+                    text += f"@{before.author.name} > {before.clean_content.split("\n", " ")} -> {after.clean_content.split("\n", " ")} {files}\n"
                     self.messages.remove(message)
             else:
                 if ctx.channel.id == message.channel.id:
-                    text += f"@{message.author.name} > {message.clean_content}\n"
+                    files = ""
+                    if message.attachments:
+                        for attachment in message.attachments:
+                            files += f"{attachment.url} "
+                    text += f"@{message.author.name} > {message.clean_content.split("\n", " ")} {files}\n"
                     self.messages.remove(message)
 
         file = discord.File(io.BytesIO(text.encode()), "log.txt")
