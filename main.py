cf9da8139de183cf321977e06c5b9cd7a1d31775
ねんねこ <nennneko5787@gmail.com>
2025-02-22 20:55:24 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def setup_hook():
     await bot.load_extension("cogs.admin")
     await bot.load_extension("cogs.ai")
     await bot.load_extension("cogs.sniper")
-    # await bot.load_extension("cogs.reply")
+    await bot.load_extension("cogs.reply")
     # await bot.load_extension("cogs.boomerang")
     await bot.load_extension("cogs.music")
     await bot.load_extension("cogs.ytdl")
