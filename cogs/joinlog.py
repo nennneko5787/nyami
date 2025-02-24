3a3e0dd2f77eea9477cd011a3a80a33179e6011a
ねんねこ <nennneko5787@gmail.com>
2025-02-24 22:02:09 +0900

    joinlog.py を更新

--- cogs/joinlog.py
+++ cogs/joinlog.py
@@ -5,5 +5,17 @@ class JoinLogCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        if member.guild.id != 1135775339377860648:
+            return
+        await self.bot.get_channel(1252406721943703683).send(f"{member.mention}" がやって来ました。")
+
+    @commands.Cog.listener()
+    async def on_raw_member_remove(self, payload: discord.RawMemberRemoveEvent):
+        if payload.guild_id != 1135775339377860648:
+            return
+        await self.bot.get_channel(1252406721943703683).send(f"{payload.user.mention}" が退出しました。")
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(JoinLog(bot))
