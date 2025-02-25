d423ec7f72e72e4955e0f5e8bec43c54c95a46dc
ねんねこ <nennneko5787@gmail.com>
2025-02-25 21:53:51 +0900

    musica.py を更新

--- cogs/musica.py
+++ cogs/musica.py
@@ -8,13 +8,13 @@ class MusicACog(commands.Cog):
 
     @commands.command("musica")
     async def musicaPlay(
-        self, ctx: commands.Context, mode: str, url: str
+        self, ctx: commands.Context, mode: str, url: str = ""
     ):
         if ctx.author.id != 1048448686914551879:
             return
         match mode:
             case "join":
-                await ctx.author.voice.connect()
+                await ctx.author.voice.channel.connect()
             case "play":
                 for i in range(1, 15):
                     await ctx.send(f"moyai{i}.music play {url}")
